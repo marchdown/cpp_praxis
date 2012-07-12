6cd320d5f6d96813fe2b21fd93ad1ed1a9aec5ba
unknown <marchdown@gmail.com>
2012-07-13 00:43:37 +0400

    done ex. 6

--- acpp1/acpp1/acpp1.cpp
+++ acpp1/acpp1/acpp1.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
 #include <string>
 
+
+void ex6(){
+
+	std::cout << "Hello, who might you be?" << std::endl;
+	
+	std::string name;
+std::cin >> name;	
+std::cout << "Nice to meet you, " << name << std::endl << "and you?" << std::endl;
+std::cin >> name;
+std::cout << "Nice to meet you too, " << name " Glorifax!"<< std::endl; 
+
+
+
+}
+
 int main()
 {
 
+	ex6();
 	// объ€вим переменную, чтоб было где держать входную информацию
 	std::string name;
 
@@ -14,6 +30,10 @@ int main()
 
 	// преобразуем или используем вход
 	const std::string greeting = "Hello, " + name + "!";
+	{const std::string goodbye = "Farewell, " + name + "!";
+	std::cout << goodbye << std::endl;
+	{const std::string goodbye = "Godspeed, " + name + "!";
+	std::cout << goodbye << std::endl;};;;;}
 	const std::string spaces(greeting.size(), ' ');
 	const std::string first(greeting.size()+4, '*');
 	const std::string second = "* "+spaces+" *";
@@ -29,7 +49,7 @@ std::cout << third <<std::endl;
 std::cout << second <<std::endl;
 
 std::cout << first <<std::endl;
-std::cout << name +", left" + " associate";
+std::cout << name +", left" + " associate"<<std::endl;
 // std::cout <<  "right " + "associate, "+ name; 
 std::cout << first <<std::endl;
 
