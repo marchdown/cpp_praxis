832f907c8c52802d370d5950e8cf26644acf2224
Pavel Vasilyev <asthmor@gmail.com>
2012-07-23 18:37:09 +0300

    Fixed Chapter 2

--- acpp2/acpp2.cpp
+++ acpp2/acpp2.cpp
@@ -9,20 +9,15 @@ int main()
 
   std::string name;
   std::cin >> name;
+  std::cout << std::endl;
+
 
   const std::string greeting = "Hello, " + name + "!";
-
-  //  std::cout << greeting << std::endl;
-  
-
-  const int pad = 1;
+  const int pad = 3;
   const int rows_needed = pad * 2 + 3;
+  const std::string::size_type columns_needed = greeting.size() + (pad + 1) * 2;
 
-  std::cout << std::endl;
-  
   int rows_printed = 0;
-const std::string::size_type columns_needed = greeting.size() + (pad + 1) * 2;
-
   while (rows_printed != rows_needed) { //invariant: already printed rows_printed rows 
     if (rows_printed == 0 or rows_printed == rows_needed-1){
       std::cout << std::string(greeting.size()+(pad + 1)*2, '*'); // invariant broken
